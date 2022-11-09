9842f7904e70af4f5be3cedac9628f0c932053d0
Desiree Mendes <desireefrost@live.ca>
2022-11-09 15:02:46 -0500

    grab text field value and push to leads array

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+
 let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
-    console.log("buttonclicked")
+    myLeads.push(inputBtn.value)
 })

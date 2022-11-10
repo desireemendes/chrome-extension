ac3ec70206792c498a8bf672f376cc89f182dc2e
Desiree Mendes <desireefrost@live.ca>
2022-11-09 22:14:15 -0500

    render list to page using for loop and textContent

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputBtn.value)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i] + " "
+}
